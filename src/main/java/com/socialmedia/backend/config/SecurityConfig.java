2b9fb60416460564cc7743cb9535d27a9410012a
Jose De Jesus <josegabrieldejesus@hotmail.com>
2026-03-01 17:58:49 -0500

    Google Oauth is working. Some service has to be modified so they match with the google Auth Pattern. Additionally, security config has to be enforced.

--- src/main/java/com/socialmedia/backend/config/SecurityConfig.java
+++ src/main/java/com/socialmedia/backend/config/SecurityConfig.java
@@ -1,22 +1,29 @@
-//TODO: DELETE WHEN USERS IMPLEMENTED
-
 package com.socialmedia.backend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final OAuth2SuccessHandler successHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth -> oauth
+                        .successHandler(successHandler)  // 🔥 connect handler
                 );
+
         return http.build();
     }
 }
